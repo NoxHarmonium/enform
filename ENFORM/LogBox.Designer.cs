2cf17b26b351b0d34898ecdc05b7edc9084079d3
Sean Dawson <sean.dawson@noxharmonium.com>
2011-09-29 08:04:09 +0000

    Added Logbox form for new logging system

---
+++ ENFORM/LogBox.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace ENFORM
+{
+    partial class frmLogBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLogBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtLogBox
+            // 
+            this.txtLogBox.Location = new System.Drawing.Point(12, 12);
+            this.txtLogBox.Multiline = true;
+            this.txtLogBox.Name = "txtLogBox";
+            this.txtLogBox.Size = new System.Drawing.Size(855, 238);
+            this.txtLogBox.TabIndex = 0;
+            // 
+            // frmLogBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 262);
+            this.Controls.Add(this.txtLogBox);
+            this.Name = "frmLogBox";
+            this.Text = "ENFORM Log";
+            this.Load += new System.EventHandler(this.LogBox_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLogBox;
+    }
+}
---
+++ ENFORM/LogBox.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ENFORM
+{
+    public partial class frmLogBox : Form
+    {
+        public frmLogBox()
+        {
+            InitializeComponent();
+        }
+
+        private void LogBox_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void AddLogEntry(string message)
+        {
+            txtLogBox.Text += "[" + DateTime.Now.ToLongTimeString() + "] - " + message + "\n";
+
+        }
+    }
+}
